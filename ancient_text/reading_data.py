ad11ae68e398b913f54056a5be84144e590c2b08
Konrad Eilers <konradeilers@Konrads-MacBook-Air.local>
2021-05-31 22:54:10 +0200

    bug

--- ancient_text/reading_data.py
+++ ancient_text/reading_data.py
@@ -3,7 +3,6 @@ from pdf2image import convert_from_path
 import glob
 import os
 import re
-from ancient_text import utils
 
 def magic_converter(document, path_to_save_text):
 
@@ -20,15 +19,3 @@ def magic_converter(document, path_to_save_text):
             with open(f'{pdf_path[:-4]}_page{pageNum}.txt', 'w') as the_file:
                 the_file.write(text)
 
-
-def atoi(text):
-    return int(text) if text.isdigit() else text
-
-def natural_keys(text):
-    return [atoi(c) for c in re.split(r'(\d+)', text)]
-
-def load_text(path):
-	files = utils.find_txt_filenames(path)
-	sorted_files = sorted(files, key = natural_keys)
-	data = utils.string_conversion(sorted_files)
-	return data
